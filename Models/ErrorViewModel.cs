4dafce5c52734bc4ecf632ab21d0ab5531ca368d
Nusrat Jahan Tuli <nusrattuli69@gmail.com>
2026-08-14 22:55:31 +0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Onudhabon_ISD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
