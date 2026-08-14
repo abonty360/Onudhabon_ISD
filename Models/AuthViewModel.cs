2040c22f1518e8c9a89d30f64ec71bb29105f8a6
Nusrat Jahan Tuli <nusrattuli69@gmail.com>
2026-08-15 00:04:42 +0600

    login register page added

---
+++ Models/AuthViewModel.cs
@@ -0,0 +1,11 @@
+namespace Onudhabon_ISD.Models
+{
+    public class AuthViewModel
+    {
+        public LoginViewModel Login { get; set; } = new LoginViewModel();
+        public RegisterViewModel Register { get; set; } = new RegisterViewModel();
+        public string ActiveTab { get; set; } = "login"; // "login" or "register"
+        public string? SuccessMessage { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
